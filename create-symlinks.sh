e68a8ab893b73618d5771ea3f14f6027fdb5588e
Paulo Janotti <pauloja@microsoft.com>
2018-04-19 12:07:20 -0700

    Add pwsh profile and make vimrc visible by default

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-ln -s ~/unix-cfg/.vimrc ~/.vimrc
- 
+ln -s ~/unix-cfg/vimrc ~/.vimrc
+ln -s ~/unix-cfg/Microsoft.PowerShell_profile.ps1 ~/.config/powershell/Microsoft.PowerShell_profile.ps1
